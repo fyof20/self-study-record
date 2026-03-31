9f95b12e2bf6617644c0f9716ef276bba9b814f2
fyof20 <2640331737@qq.com>
2026-03-31 14:24:54 +0800

    forth of mit

--- mini_git/src/cli.rs
+++ mini_git/src/cli.rs
@@ -47,7 +47,7 @@ enum Comand {
         allow_empty: bool,
     },
     ///查看当前状态
-    Stauts,
+    Status,
     ///log 现实提交历史
     #[clap(group = ArgGroup::new("sub").required(false))]//让clap知道这个子命令有两个互斥的参数
     Log {
@@ -76,13 +76,79 @@ enum Comand {
 
         ///显示当前分支
         #[clap(long, action, group="sub")]
-        show: bool,
-    }
+        show_current: bool,
+    },
 
     ///切换分支
     Switch {
         ///要切换的分支名称
-        branch_name: String,
+        #[clap(requried_unless_present="create")]//该字段是必需的，除非存在 "create" 参数
+        branch_name: Option<String>,
 
+        ///创建并切换到新分支
+        #[clap(short, long, group="sub")]
+        create: Option<String>,
 
+        ///是否允许切换到commit(分离head状态)
+        #[clap(short,long,action,default_value_t=false,group="sub")]
+        detach: bool,
+    },
+    ///restore
+    Restore {
+        ///要恢复的文件
+        #[clap(required = true)]
+        path: Vec<String>,
+
+        ///sorce
+        #[clap(long, short)]
+        source: Option<String>,
+
+        ///worktree
+        #[clap(action, long, short = 'W')]
+        worktree: bool,
+
+        ///staged
+        #[clap(action, long, short = 'S')]
+        staged: bool,
+    },
+    ///Merge
+    Merge {
+        ///要合并的分支
+        #[clap(required = true)]
+        branch: String,
+    },
+}
+pub fn handle_command() {
+    let cli = Cli::parse(); //解析命令行参数，生成一个Cli结构体实例
+    match cli.command {
+        Command::Init => cmd::init()。expect("初始化失败"),
+        Command::Add { files, all, update } => cmd::add(files, all, update),
+        Command::Add { files, all, update } => cmd::add(files, all, update),
+        Command::Rm { files, cached, recursive } => cmd::rm(files, cached, recursive).expect("删除失败"),
+        Command::Commit { messages, allow_empty } => cmd::commit(messages, allow_empty),
+        Command::Status => cmd::status(),
+        Command::Log { all, number } => cmd::log(all, number),
+        Command::Branch { new_branch, commit_hash, list, delete, show_current } => {
+            cmd::branch(new_branch, commit_hash, list, delete, show_current)
+        },
+        Command::Switch { branch_name, create, detach } => {
+            cmd::switch(branch_name, create, detach)
+        },
+        Command::Restore { path, source, mut worktree, staged } => {
+            // 未指定stage和worktree时，默认操作worktree
+            // 指定 --staged 将仅还原index
+            if !staged {
+                worktree = true;
+            }
+            // 未指定source 且 --staged，默认操作HEAD，否则从index中恢复（就近原则）
+            /*
+                If `--source` not specified, the contents are restored from `HEAD` if `--staged` is given,
+                otherwise from the [index].
+            */
+            cmd::restore(path, source, worktree, staged);
+        }
+        Command::Merge { branch } => {
+            cmd::merge(branch)
+        },
+    }
 }
